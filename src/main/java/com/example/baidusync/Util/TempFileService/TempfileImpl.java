9715487dcc6c2775b4c3fab01b5ced7a4fd82a3d
keson-ym <yeungluhyun@163.com>
2022-09-14 08:44:38 +0800

    分片

--- src/main/java/com/example/baidusync/Util/TempFileService/TempfileImpl.java
+++ src/main/java/com/example/baidusync/Util/TempFileService/TempfileImpl.java
@@ -5,9 +5,12 @@ import com.example.baidusync.Admin.Entity.FileSetting;
 import com.example.baidusync.Admin.Service.FileSettingMapper.FileSettingMapping;
 import com.example.baidusync.Util.NetDiskSync.RequestNetDiskImpl;
 import com.example.baidusync.Util.NetDiskSync.RequestNetDiskService;
+import org.apache.commons.io.FileUtils;
 
 import javax.annotation.Resource;
 import java.io.File;
+import java.io.RandomAccessFile;
+import java.math.BigDecimal;
 
 /**
  * @author 杨名 （字 露煊）
@@ -54,14 +57,59 @@ public class TempfileImpl {
         if (!cacheDir.exists() && !cacheDir.isDirectory()){
             cacheDir.mkdir();
         }
+        //获取文件，计算大小
+        String filePath = "";
+        File file = new File(filePath);
+        String name = filePath.split("/")[filePath.length() - 1];
+        Long fileSize = FileUtils.sizeOf(file);
+        if (fileSize <= SIZE){
+            //直接上传
+        }else{
+            BigDecimal total = new BigDecimal(fileSize).divide(new BigDecimal(SIZE),0,BigDecimal.ROUND_UP);
+            Integer count = total.intValue(); //分多少片
+            RandomAccessFile raf = null;
+            try {
+                raf = new RandomAccessFile(file,"r");
+                long maxSize = fileSize / count ;
+                long offSet = 0L;
+                for (int i= 0 ; i< count ; i++){
+                    long begin = offSet;
+                    long end = (i+1) * maxSize;
+                    offSet = write(name,raf,i,begin,end);
+                }
+            }catch (Exception e){
+
+            }
+        }
 
     }
 
 
     /**
      * 切分并写文件
+     * @param filename 文件名
+     * @param file 文件片
+     * @param index 索引
+     * @param end 索引
      */
+    private Long write(String filename,RandomAccessFile file,int index,long begin,long end){
+        long enPointer = 0L; //结尾指针
+        try {
+            RandomAccessFile in = file;
+            RandomAccessFile out = new RandomAccessFile(new File(filename +"_"+index+".tmp"),"rw");
+            byte[] bytes = new byte[Math.toIntExact(SIZE)];
+            int n = 0 ;
+            in.seek(begin);
+            while (in.getFilePointer() <= end && (n = in.read(bytes)) != -1){
+                out.write(bytes,0,n);
+            }
+            enPointer = in.getFilePointer();
+            out.close();
+        }catch (Exception e){
 
+        }
+        return enPointer;
+    }
 
     /**
      * 读文件
